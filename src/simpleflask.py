763371d68cb8cc5030b56b04e6d14eb65f60934d
varennak <varennak@gmail.com>
2021-01-15 10:49:15 -0800

    added DELETE request functionality

--- src/simpleflask.py
+++ src/simpleflask.py
@@ -59,11 +59,22 @@ def get_users():
         return resp
 
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return({})
-    return users
+    if request.method == 'GET':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return({})
+        return users
+    elif request.method == 'DELETE':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    resp = jsonify(success=True)
+                    return resp
+            resp = jsonify("object not found")
+            resp.status_code = 404
+            return resp
