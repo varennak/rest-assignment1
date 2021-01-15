46676aed4eac49c2b7af69d7294960e306fe752e
varennak <varennak@gmail.com>
2021-01-14 18:50:09 -0800

    "first commit"

---
+++ venv/src/simpleflask.py
@@ -0,0 +1,43 @@
+from flask import Flask
+app = Flask(__name__)
+
+users = {
+   'users_list':
+   [
+      {
+         'id': 'xyz789',
+         'name': 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id': 'abc123',
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id': 'ppp222',
+         'name': 'Mac',
+         'job': 'Professor',
+      },
+      {
+         'id': 'yat999',
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id': 'zap555',
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World'
+
+
+@app.route('/users')
+def get_users():
+    return users
