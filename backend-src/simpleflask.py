4831a50171c42915512eb383dc6c9c3f6ea1dcfd
Varenna Kay <38743258+varennak@users.noreply.github.com>
2021-01-15 14:08:10 -0800

    6.1 changes

---
+++ backend-src/simpleflask.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+users = {
+    'users_list':
+    [
+        {
+            'id': 'xyz789',
+            'name': 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id': 'abc123',
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id': 'ppp222',
+            'name': 'Mac',
+            'job': 'Professor',
+        },
+        {
+            'id': 'yat999',
+            'name': 'Dee',
+            'job': 'Aspiring actress',
+        },
+        {
+            'id': 'zap555',
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
+}
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World'
+
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job = request.args.get('job')
+        if search_username and search_job:
+            return find_user_name_job(search_username, search_job)
+        elif search_username:
+            return find_user_name(search_username)
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        resp.status_code = 201
+        return resp
+
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def get_user(id):
+    if request.method == 'GET':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return({})
+        return users
+    elif request.method == 'DELETE':
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    resp = jsonify()
+                    resp.status_code = 204
+                    return resp
+            resp = jsonify("User not found with given ID")
+            resp.status_code = 404
+            return resp
+
+
+def find_user_name_job(name, job):
+    subdict = {'users_list': []}
+    for user in users['users_list']:
+        if user['name'] == name and user['job'] == job:
+            subdict['users_list'].append(user)
+    return subdict
+
+
+def find_user_name(name):
+    subdict = {'users_list': []}
+    for user in users['users_list']:
+        if user['name'] == name:
+            subdict['users_list'].append(user)
+    return subdict
