f5befa1dd07f21e6e940ede9a930b7267762bdc1
Varenna Kay <38743258+varennak@users.noreply.github.com>
2021-01-15 13:48:05 -0800

    first frontend changes

---
+++ frontend-src/MyApp.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Table from './Table'
+import Form from './Form'
+
+function MyApp() { 
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+       return i !== index
+    });
+    setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+     });
+  }, [] );
+
+ return (
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form handleSubmit={updateList} />
+  </div>
+  )
+
+}
+
+export default MyApp;
